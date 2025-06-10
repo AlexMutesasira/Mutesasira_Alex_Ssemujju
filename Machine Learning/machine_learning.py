1dc69880cb0f8c5aed7fd7953d724c49c85c863b
AlexMutesasira <mutesasira.ssemujju@gmail.com>
2025-06-10 16:55:25 +0300

    Machine learning

---
+++ Machine Learning/machine_learning.py
@@ -0,0 +1,31 @@
+# Machine learning 
+# Introduction to machine learning
+# Machine learning is a tool of data science
+# Machine learning is a subset of artificial intelligence
+
+
+# Application of Machine learning
+# 1. Image analysis
+# 2. Speech recognition
+# 3. Disease prediction
+# 4. Stock market
+# 5. Fraud detection
+
+# Types of Machine Learning
+# 1.Supervised learning --labelled dataset, structured
+'''
+    Common algorithms in Supervised learning
+    1.Linear Regression
+    2.Logistic Regression
+    3.Decision Tree
+    4.Support Vector Machine (SVM)
+    5.K Nearest Neighbor (KNN)
+    6.Random Forest
+    
+    
+    Deep learning (CNN) - Image classification
+    
+'''
+# 2.Unsupervised learning
+# 3.Semi supervised learning
+# 4.Reinforcement learning
