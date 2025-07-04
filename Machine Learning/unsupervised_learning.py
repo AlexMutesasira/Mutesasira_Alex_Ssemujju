c98793390a2b40d647194258815b966a198b91f2
AlexMutesasira <mutesasira.ssemujju@gmail.com>
2025-07-04 15:32:59 +0300

    Added

---
+++ Machine Learning/unsupervised_learning.py
@@ -0,0 +1,30 @@
+# unlabelled data or unstructured data
+# What is unsupervised learning ? unlabelled or unstructured data.. pattern recognition 
+
+# Characteristics of unsupervised learning 
+# No labelled data provided 
+# The model identify hidden patterns , structures or relation in the data
+# Used for EDA exploratory data analysis
+# Can handle large volumes of data
+
+# TEchnique of unsupervised learning
+# Clustering
+# Dimensionality reduction
+# Anomaly detection
+# Association rule Learning
+
+# Clustering
+# Algorithms for clustering
+# K-means clustering
+# Hierarchical clustering
+# Density based spartial clustering
+
+# Advantages of unsupervised learning
+# used for discovering unknown patterns
+# No need for labelled data
+# Can handle large volumes and comlpex of data
+# Flexible for EDA
+
+
+# Example One : Real world problems
+# Create a dataset of customers based on their annual income and spending
